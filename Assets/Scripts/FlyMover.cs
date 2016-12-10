a7f781d2a7ad05d6b599bbd0615f3f2c7f704a9d
Jordy van Opstal <jordy_van_opstal@live.nl>
2016-12-10 15:58:22 +0100

    Fixed z rotation bug on fly controller

--- Assets/Scripts/FlyMover.cs
+++ Assets/Scripts/FlyMover.cs
@@ -4,24 +4,24 @@ using UnityEngine.Networking;
 public class FlyMover : MonoBehaviour
 {
     public float ConstantForwardSpeed = 5;
-    public float RotationSpeed = 5  ;
+    public float RotationSpeed = 5;
     //components
     private Rigidbody _rigidbody;
 
     //input
-    private Quaternion _rotation;
-	void Start()
-	{
-	    _rigidbody = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
+    private float horizontal = 0;
+    private float vertical = 0;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        float hor = Input.GetAxis("Horizontal")*RotationSpeed;
-        float vert = Input.GetAxis("Vertical")*RotationSpeed;
-
-        _rotation = Quaternion.Euler(vert, hor, 0);
+        horizontal += Input.GetAxis("Horizontal")*RotationSpeed;
+        vertical += Input.GetAxis("Vertical")*RotationSpeed;
     }
 
     void FixedUpdate()
@@ -35,7 +35,7 @@ public class FlyMover : MonoBehaviour
     /// </summary>
     private void ApplyForwardMovement()
     {
-        _rigidbody.AddForce(transform.forward * ConstantForwardSpeed);
+        _rigidbody.AddForce(transform.forward*ConstantForwardSpeed);
     }
 
     /// <summary>
@@ -43,7 +43,6 @@ public class FlyMover : MonoBehaviour
     /// </summary>
     private void ApplyRotation()
     {
-        transform.localRotation *= _rotation;
-
+        transform.localRotation = Quaternion.Euler(vertical, horizontal, 0);
     }
 }
