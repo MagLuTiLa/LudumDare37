90a455958621a1fc12ab6093aaefdb22f9f95d5c
Jordy van Opstal <jordy_van_opstal@live.nl>
2016-12-10 16:56:06 +0100

    Fly bounces when it collides with an object now

--- Assets/Scripts/FlyCollisionController.cs
+++ Assets/Scripts/FlyCollisionController.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlyCollisionController : MonoBehaviour {
+public class FlyCollisionController : MonoBehaviour
+{
+    public float BounceForce = 10;
+    private Rigidbody _rigidbody;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 direction = collision.contacts[0].point - transform.position;
+        direction = -direction.normalized;
+        _rigidbody.AddForce(direction * BounceForce);
+    }
 }
