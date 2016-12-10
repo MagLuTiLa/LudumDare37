c4d60e2194543b1efd5dc1362feaf84b8cb2c9a2
kamipenguin <darkroxy@Live.nl>
2016-12-10 23:22:31 +0100

    Added Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text timerText;
+    private string timerDisplay;
+    private bool stopTimer;
+    private float beginVal;
+    private float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        beginVal = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        DisplayText();
+    }
+
+    void ResetTimer()
+    {
+        timer = beginVal;
+    }
+
+    void DisplayText()
+    {
+        int min = Mathf.FloorToInt(timer / 60F);
+        int sec = Mathf.FloorToInt(timer - min * 60);
+        timerDisplay = string.Format("{0:0}:{1:00}", min, sec);
+
+        timerText.text = timerDisplay;
+    }
+}
