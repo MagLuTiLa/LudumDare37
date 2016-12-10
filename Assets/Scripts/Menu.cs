6370da1fdc30f56a6d1e93d59ad9f38107293922
kamipenguin <darkroxy@Live.nl>
2016-12-10 22:36:46 +0100

    Added Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public Image background;
+
+    void Start()
+    {
+        background.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+    }
+
+    public void ClickedOnStart()
+    {
+        Application.LoadLevel("The Room");
+    }
+}
