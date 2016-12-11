c22f9ae471b1f86bede5db1e54ca572d27ddc6e3
Sam <sam_mie_sam@hotmail.com>
2016-12-11 16:55:30 +0100

    now with files

---
+++ Assets/Scripts/FanRotator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRotator : MonoBehaviour
+{
+    //Angular speed if turned on
+    public float MaxAngularVelocity = .15f;
+    public Vector3 Torque = new Vector3(0, 1, 0);
+    //Percentual dampening when turned off;
+    public bool active = true;
+
+    //components
+    private Rigidbody _rigidbody;
+
+    public void Toggle()
+    {
+        active = !active;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.maxAngularVelocity= MaxAngularVelocity;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _rigidbody.maxAngularVelocity = MaxAngularVelocity;
+        if (active)
+            _rigidbody.AddTorque(Torque);
+    }
+}
