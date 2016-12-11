3777a9d2993023d35c7a466ca82580b3bf39735f
Asteryz <asteryz@gmail.com>
2016-12-11 14:07:36 +0100

    Added deathcam

--- Assets/Scripts/Swat.cs
+++ Assets/Scripts/Swat.cs
@@ -9,5 +9,7 @@ public class Hazard : MonoBehaviour {
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.useGravity = false;
         rb.velocity = new Vector3();
+
+        GameObject.Find("DeathCam").GetComponent<Camera>().enabled = true;
     }
 }
