35966d524b39cc21e4994e29d09046da4cce38fa
maguell <maglutila@gmail.com>
2016-12-11 23:59:25 +0100

    Can now use both LMB and space

--- Assets/CreditScript.cs
+++ Assets/CreditScript.cs
@@ -17,7 +17,7 @@ public class CreditScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetKey(KeyCode.Space))
             SceneManager.LoadScene("Menu");
     }
 
