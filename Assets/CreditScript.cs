03dd220bcc7ed7b42a7b4a9f186f32134b98e5ec
LAPTOP\Geert Beuneker <g.beuneker@gmail.com>
2016-12-11 18:35:04 +0100

    Added credits scene

---
+++ Assets/CreditScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<AudioSource>().time = 8.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
