18044fc2e41ce117f0afa0905b513c24b688eff7
Sam <sam_mie_sam@hotmail.com>
2016-12-11 22:01:08 +0100

    Fans sound fixed

---
+++ Assets/Scripts/FanCollapse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanCollapse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    //components
+    private Rigidbody _rigidbody;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider != null && collision.collider.tag == "Player")
+        {
+            _rigidbody.useGravity = true;
+            _rigidbody.constraints = 0;
+        }
+    }
+}
